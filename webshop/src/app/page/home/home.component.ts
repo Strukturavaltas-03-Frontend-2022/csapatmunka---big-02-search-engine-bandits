c6a3407d1cf9ba0c8275ee89af79a0c65f2d5050
k3csk3 <szilagyi.viktor89@gmail.com>
2022-12-01 18:09:40 +0100

    first step

--- webshop/src/app/page/home/home.component.ts
+++ webshop/src/app/page/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Bill } from 'src/app/model/bill';
+import { Product } from 'src/app/model/product';
+import { BillService } from 'src/app/service/bill.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  productService: ProductService = inject(ProductService);
+  billService: BillService = inject(BillService);
 
-  constructor() { }
+  productList$: Observable<Product[]> = this.productService.getAll();
+  billList$: Observable<Bill[]> = this.billService.getAll();
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
