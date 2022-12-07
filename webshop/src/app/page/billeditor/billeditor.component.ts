eacd0db52b1f1f6885b0bee6250cc2050d372128
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-12-07 10:37:36 +0100

    New bill kész

--- webshop/src/app/page/billeditor/billeditor.component.ts
+++ webshop/src/app/page/billeditor/billeditor.component.ts
@@ -2,7 +2,9 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 import { Bill } from 'src/app/model/bill';
+import { Order } from 'src/app/model/order';
 import { BillService } from 'src/app/service/bill.service';
+import { OrderService } from 'src/app/service/order.service';
 
 @Component({
   selector: 'app-billeditor',
@@ -11,6 +13,7 @@ import { BillService } from 'src/app/service/bill.service';
 })
 export class BilleditorComponent implements OnInit {
   billService: BillService = inject(BillService);
+  orderService: OrderService = inject(OrderService);
   ar: ActivatedRoute = inject(ActivatedRoute);
   router: Router = inject(Router);
 
@@ -19,14 +22,27 @@ export class BilleditorComponent implements OnInit {
   );
 
   bill: Bill = new Bill();
+  orders: Order[] = [];
   checked: boolean = false;
 
   constructor() {}
 
+  statusList: any[] = [
+    { key: 'paid', title: 'This bill is paid' },
+    { key: 'new', title: 'This bill is not paid yet' },
+  ];
+
   ngOnInit(): void {
-    this.bill$.subscribe((bill) => {
-      this.bill = bill;
+    this.ar.params.subscribe((params) => {
+      if(!params['id']){
+        return;
+      }
+      this.bill$.subscribe((bill) => {
+        this.bill = bill;
+      });
     });
+
+    this.orderService.getAll().subscribe(orders => this.orders = orders);
   }
 
   onChecked(): void {
